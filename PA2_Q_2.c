8f6d98fd4a9cdd7d68b05e979d7a767a9d3d6e9b
A.M.Nolan <username@users.noreply.github.com>
2019-10-15 21:12:08 -0700

    Added comments

--- PA2_Q_2.c
+++ PA2_Q_2.c
@@ -8,6 +8,7 @@
 // global variables
 int a, b;
 
+// fun() simply adds 10 to a global variable
 int fun(){
     a = a + 10;
     return a;
@@ -15,12 +16,16 @@ int fun(){
 
 int main()
 {
-    
+    // start with 10
     a = 10;
+    // associativity left to right initially a is 10, so (10 + (10+10)) = 30
     b = a + fun();
     printf("With the function call on the right, ");
     printf(" b is: %d\n", b);
+
+    // again initially starting with 10
     a = 10;
+    // associativity states left to right, function addds the value right away (10 + 10) + (10 + 10) = 40
     b = fun() + a;
     printf("With the function call on the left, ");
     printf(" b is: %d\n", b);
